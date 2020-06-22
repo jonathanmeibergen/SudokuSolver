654c293192e9b2445fea203dbc2b139eb3e71731
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-06-22 15:24:24 +0200

    Slechte code

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -13,7 +13,11 @@ namespace SudokuSolver.Logics
 
         public int[][] Solve(int[][] sudoku)
         {
-            //merge this!!!
+
+            int[] temp = new int[6];
+            Console.WriteLine(temp.Count());
+            Console.WriteLine(temp.Length);
+            Console.WriteLine(temp.Rank);
             return sudoku;
         }
 
