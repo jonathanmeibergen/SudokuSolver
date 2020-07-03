4272c4ec89e7a8c31630c5ececc4700e6371dbef
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-07-03 13:21:11 +0200

    Added a pattern Sudoku

--- SudokuSolver/Models/Sudokus.cs
+++ SudokuSolver/Models/Sudokus.cs
@@ -78,6 +78,23 @@ namespace SudokuSolver.Models
                         new int[9] {0,0,8,5,0,0,0,1,0 },
                         new int[9] {0,9,0,0,0,0,4,0,0 }
                     }
+                },
+                new Sudoku
+                {
+                    SudokuId = 4,
+                    Name = "Pattern",
+                    Cells = new int[][]
+                    {
+                        new int[9] {0,0,9,0,3,0,4,0,0 },
+                        new int[9] {0,4,0,1,0,9,0,8,0 },
+                        new int[9] {3,0,0,0,7,0,0,0,2 },
+                        new int[9] {0,5,0,0,0,0,0,3,0 },
+                        new int[9] {8,0,2,0,0,0,7,0,5 },
+                        new int[9] {0,7,0,0,0,0,0,4,0 },
+                        new int[9] {4,0,0,0,6,0,0,0,8 },
+                        new int[9] {0,8,0,7,0,4,0,5,0 },
+                        new int[9] {0,0,6,0,8,0,1,0,0 }
+                    }
                 }
             };
             return sudokuList as IEnumerable<Sudoku>;
